7aa0f3b3136a631effde05663688f374b65b91b6
StephenLeeDev <stephen.lee.dev@gmail.com>
2023-05-17 11:30:50 +0900

    Refactor : Refactor GoogleSignIn Environment

--- lib/presentation/view/screen/sign_in/sign_in_screen.dart
+++ lib/presentation/view/screen/sign_in/sign_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_hive/data/repository/remote/auth/social_sign_in/google_sign_in_api.dart';
+import 'package:todo_hive/presentation/util/snackbar/snackbar_util.dart';
 import 'package:todo_hive/presentation/view/widget/common/button/custom_elevated_button.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -14,7 +15,7 @@ class SignInScreen extends StatelessWidget {
       body: Center(
         child: CustomElevatedButton(
           onPressed: () async {
-            googleSignIn();
+            googleSignIn(context: context);
           },
           text: 'Google SignIn',
         ),
@@ -22,19 +23,14 @@ class SignInScreen extends StatelessWidget {
     );
   }
 
-  Future googleSignIn() async {
-    try {
-      var user = await GoogleSignInApi.login();
-      if (user != null) {
-        debugPrint('googleSignIn');
-        debugPrint(user.displayName);
-        debugPrint(user.email);
-        debugPrint(user.id);
-        debugPrint(user.serverAuthCode);
-        debugPrint(user.photoUrl);
-      }
-    } catch (e) {
-      debugPrint('googleSignIn error : ${e.toString()}');
+  Future googleSignIn({ required BuildContext context }) async {
+    var user = await GoogleSignInApi.signIn();
+    if (user != null) {
+      // TODO : Implement sign-up/sign-in features with remote server.
+      // TODO : code == 200, Not yet sign-up. Proceed sign-up process.
+      // TODO : code == 409, Already sign-up. Just proceed sign-in process directly.
+    } else {
+      if (context.mounted) showSnackBar(context: context, text: "It went something wrong.\nPlease try again.");
     }
   }
 }
